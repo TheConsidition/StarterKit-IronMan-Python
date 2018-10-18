7b93d49ed88faa99cabb412bd83e6060f64c2732
Fredrik Ek <fredrik.ek.s@gmail.com>
2018-10-18 17:36:10 +0200

    cleanup in python files

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import requests
 import time
 
+
 class API:
     _base_api_path = "http://theconsidition.se/considition/ironman/"
     _api_key = ""
@@ -18,130 +19,138 @@ class API:
         self._number_of_waterstreams = numberOfWaterstreams
         self._number_of_powerups = numberOfPowerups
 
-    #Gets the gamestate for a given game_id.
-    #Returns: The gamestate as JSON
+    # Gets the gamestate for a given game_id.
+    # Returns: The gamestate as JSON
     def get_game(self, game_id):
         print("Getting game: " + game_id)
-        r = requests.get(self._base_api_path+"games/"+game_id+'/'+self._api_key)
+        r = requests.get(self._base_api_path + "games/" + game_id + '/' + self._api_key)
         response = r.json()
-        if (response["success"] != True):
+        if not response["success"]:
             print(response["message"])
         else:
             return response
 
-    #Creates a new game, with a specified number of max players,
-    #waterstreams, elevations, and powerups.
-    #Returns: The game_id of the new game
+    # Creates a new game, with a specified number of max players,
+    # waterstreams, elevations, and powerups.
+    # Returns: The game_id of the new game
     def init_game(self):
-        r = requests.post(self._base_api_path+"games", json = { "ApiKey" :self._api_key, "MaxPlayers" : self._max_players, "Map" : self._map, "NumberOfStreams" : self._number_of_waterstreams, "NumberOfElevations" : self._number_of_elevations, "NumberOfPowerups" : self._number_of_powerups})
+        r = requests.post(self._base_api_path + "games",
+                          json={"ApiKey": self._api_key, "MaxPlayers": self._max_players, "Map": self._map,
+                                "NumberOfStreams": self._number_of_waterstreams,
+                                "NumberOfElevations": self._number_of_elevations,
+                                "NumberOfPowerups": self._number_of_powerups})
         response = r.json()
-        if (response["success"] != True):
+        if not response["success"]:
             print(response["message"])
         else:
             game_id = response["gameId"]
             print("Created new game: " + game_id)
             return game_id
 
-    #Joins a game
-    #Returns: The gamestate after the new player has been inserted
+    # Joins a game
+    # Returns: The gamestate after the new player has been inserted
     def join_game(self, game_id):
-        r = requests.post(self._base_api_path+"games/"+game_id+"/join", json = { "ApiKey" : self._api_key })
+        r = requests.post(self._base_api_path + "games/" + game_id + "/join", json={"ApiKey": self._api_key})
         response = r.json()
-        if (response["success"] != True):
+        if not response["success"]:
             print(response["message"])
         else:
             print("Joined game: " + response["gameState"]["gameId"])
             return response
 
-    #Readies up for a game
-    #Returns: The gamestate after the player has readied up
+    # Readies up for a game
+    # Returns: The gamestate after the player has readied up
     def ready_up(self, game_id):
         print("Readying up!")
-        r = requests.post(self._base_api_path+"games/"+game_id+"/ready", json = { "ApiKey" : self._api_key})
+        r = requests.post(self._base_api_path + "games/" + game_id + "/ready", json={"ApiKey": self._api_key})
         response = r.json()
-        if (response["success"] != True):
+        if not response["success"]:
             print(response["message"])
         else:
             return response
 
-    #Creates, joins, and readies up for a game
-    #Returns: The ID of the started game
+    # Creates, joins, and readies up for a game
+    # Returns: The ID of the started game
     def initiate_one_player_game(self):
         game_id = self.init_game()
         joined_response = self.join_game(game_id)
         readied_response = self.ready_up(game_id)
         return readied_response["gameState"]["gameId"]
 
-    #Continously try to ready up for a game
-    #To be used when joining games with more than one player
-    #Returns: The gamestate after all of the players have successfully readied up
+    # Continously try to ready up for a game
+    # To be used when joining games with more than one player
+    # Returns: The gamestate after all of the players have successfully readied up
     def try_ready_for_game(self, game_id):
-        print('Readying up!');    
+        print('Readying up!');
         readied_response = self.ready_up(game_id)
-        while (readied_response == None):
+        while readied_response is None:
             print("Trying to ready up")
             time.sleep(2)
             readied_response = self.ready_up(game_id)
         return readied_response
 
-    #Makes a move in a given direction with a given speed
-    #Returns: The updated gamestate
+    # Makes a move in a given direction with a given speed
+    # Returns: The updated gamestate
     def make_move(self, game_id, direction, speed):
         print("Attempting to makeMove with speed: " + speed + " and direction: " + direction)
-        r = requests.post(self._base_api_path+"games/"+game_id+"/action/move", json = { "ApiKey" : self._api_key, "Type" : "move", "Speed" : speed, "Direction" : direction})
+        r = requests.post(self._base_api_path + "games/" + game_id + "/action/move",
+                          json={"ApiKey": self._api_key, "Type": "move", "Speed": speed, "Direction": direction})
         response = r.json()
-        if (response["success"] != True):
+        if not response["success"]:
             print(response["message"])
         else:
             return response
 
-    #Takes a step in a given direction
-    #Returns: The updated gamestate
+    # Takes a step in a given direction
+    # Returns: The updated gamestate
     def step(self, game_id, direction):
         print("Attempting to step in direction: " + direction)
-        r = requests.post(self._base_api_path+"games/"+game_id+"/action/step", json = { "ApiKey" : self._api_key, "Direction" : direction})
-        if (r != None):
+        r = requests.post(self._base_api_path + "games/" + game_id + "/action/step",
+                          json={"ApiKey": self._api_key, "Direction": direction})
+        if r is not None:
             response = r.json()
-            if (response["success"] != True):
+            if not response["success"]:
                 print(response["message"])
             else:
                 return response
 
-    #Rests for 1 turn
-    #Returns: The updated gamestate
+    # Rests for 1 turn
+    # Returns: The updated gamestate
     def rest(self, game_id):
         print("Attempting to rest!")
         r = requests.post(self._base_api_path + "games/" + game_id + "/action/rest", json={"ApiKey": self._api_key})
         response = r.json()
-        if (response["success"] != True):
+        if not response["success"]:
             print(response["message"])
         else:
             return response
 
-    #Uses a chosen powerup
+    # Uses a chosen powerup
     def use_powerup(self, game_id, powerup_name):
         print("Attempting to use powerup: " + powerup_name)
-        r = requests.post(self._base_api_path + "games/" + game_id + "/action/usepowerup", json={"ApiKey": self._api_key, "Name" : powerup_name})
+        r = requests.post(self._base_api_path + "games/" + game_id + "/action/usepowerup",
+                          json={"ApiKey": self._api_key, "Name": powerup_name})
         response = r.json()
-        if (response["success"] != True):
+        if not response["success"]:
             print(response["message"])
         else:
             return response
 
-    #Drops a chosen powerup
+    # Drops a chosen powerup
     def drop_powerup(self, game_id, powerup_name):
         print("Attempting to drop powerup: " + powerup_name)
-        r = requests.post(self._base_api_path + "games/" + game_id + "/action/droppowerup", json={"ApiKey": self._api_key, "Name" : powerup_name})
+        r = requests.post(self._base_api_path + "games/" + game_id + "/action/droppowerup",
+                          json={"ApiKey": self._api_key, "Name": powerup_name})
         response = r.json()
-        if (response["success"] != True):
+        if not response["success"]:
             print(response["message"])
         else:
             return response
 
-    #Ends previous active games
+    # Ends previous active games
     def end_previous_games_if_any(self):
         print("Attempting to end previous games if any.")
         r = requests.delete(self._base_api_path + "games", json={"ApiKey": self._api_key})
         response = r.json()
-        if (response["success"] != True):
+        if not response["success"]:
             print(response["message"])
--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from api import API
 import random
 import sys
 
-#TODO : Insert your API-key here
+# TODO : Insert your API-key here
 _api_key = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
-#Specify your API-key number of players per game),
+# Specify your API-key number of players per game),
 # mapname, and number of waterstreams/elevations/powerups here
 _api = API(_api_key, 1, "standardmap", 10, 10, 10)
 
-#A "solution" that takes a step in a random direction every turn
+
+# A "solution" that takes a step in a random direction every turn
 def solution(game_id):
     initial_state = _api.get_game(game_id)
-    if (initial_state["success"] == True):
+    if initial_state["success"]:
         state = initial_state["gameState"]
         tiles = state["tileInfo"]
         current_player = state["yourPlayer"]
@@ -37,17 +38,18 @@ def solution(game_id):
 
 def main():
     game_id = ""
-    #If no gameID is specified as parameter to the script,
-    #Initiate a game with 1 player on the standard map
-    if (len(sys.argv) == 1):
-        _api.end_previous_games_if_any() #Can only have 2 active games at once. This will end any previous ones.
+    # If no gameID is specified as parameter to the script,
+    # Initiate a game with 1 player on the standard map
+    if len(sys.argv) == 1:
+        _api.end_previous_games_if_any()  # Can only have 2 active games at once. This will end any previous ones.
         game_id = _api.init_game()
         joined_game = _api.join_game(game_id)
         readied_game = _api.try_ready_for_game(game_id)
-        if (readied_game != None):
+        if readied_game is not None:
             print("Joined and readied! Solving...")
             solution(game_id)
     else:
         game_id = sys.argv[1]
+
 
 main()
